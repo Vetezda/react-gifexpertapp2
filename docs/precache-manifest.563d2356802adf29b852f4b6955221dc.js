2d13a18782746e4db43aa840f04de4f38898e9e1
Joseph Lopez <josephbustoslopez@gmail.com>
2022-02-22 16:09:25 -0600

    docs agregado

---
+++ docs/precache-manifest.563d2356802adf29b852f4b6955221dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54801465038a67ce0ffcda0086afca05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4071273724fc6e38190",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4071273724fc6e38190",
+    "url": "/static/js/main.8a3f2ae1.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
